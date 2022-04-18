aaa52e55cf22ae057cbf9d328299672c028047c8
Hugo Machado Rodrigues <rodrigues.machado.hugo@gmail.com>
2022-04-18 20:08:29 -0300

    Atualizando busca por mosaico de candidatos

--- code/s01_loop_matriz.R
+++ code/s01_loop_matriz.R
@@ -3,12 +3,12 @@ library(terra)
 
 # Carregando raster de covariaveis
 r_to_crop<-terra::rast("./data/covariaveis_50m.tif")
-
+names(r_to_crop)
 # Plotando o MDE
 plot(r_to_crop$dem)
 
 # Separando os rasteres de MDE e SAGA no objeto "r"
-r<-r_to_crop[[c("dem","saga_wetness_index")]]
+r<-r_to_crop[[c("dem","saga_wetness_index","amb_pedo","aspect")]]
 
 # Chacando os nomes
 names(r)
@@ -17,7 +17,7 @@ names(r)
 r <- terra::aggregate(r, 10)
 
 # Setando variavel "n" como 10. Esse valor refere-se ao numero de linhas e colunas
-n <- 10
+n <- 5
 
 # Get the starting cells of interest
 rows <- seq(1, nrow(r), by=n)
@@ -46,7 +46,7 @@ x <- lapply(1:nrow(xy), function(i) {
 e <- lapply(x, \(i) ext(i) |> as.polygons()) |> vect()
 
 # Plotando o raster de MDE
-plot(r[[1]])
+plot(r[[3]])
 
 # Plotando o bloco de linhas
 lines(e, col="blue", lwd=2)
@@ -71,7 +71,7 @@ gower_list_df<-as.numeric(gower_list_df[,])
 # Chechando os valores de cada entrada
 gower_list_df
 
-x_gower_ordered <- sort(unlist(gower_list_df), decreasing = FALSE,na.last = TRUE,index.return = TRUE)
+x_gower_ordered <- sort(unlist(gower_list_df), decreasing = FALSE,na.last = NA,index.return = TRUE)
 
 # Juntando os subrasters "x" com os valores de gower respectivos em um objeto
 x_gower_2<- cbind(x, x_gower_ordered$x,x_gower_ordered$ix)
@@ -81,10 +81,10 @@ candidatos <- (x_gower_2[,c(1)][x_gower_2[,c(2)] <= 0.25])
 
 candidatos<-candidatos[!sapply(candidatos,is.null)]
 
-teste <- do.call(terra::mosaic, candidatos[1:25])
-plot(teste)
+teste <- do.call(terra::mosaic, candidatos[c(1:length(candidatos))])
 
-plot(r$dem)
-plot(teste$dem)
+par(mfrow = c(1,1))
+terra::plot(r$saga_wetness_index,range = c(5,14))
+terra::plot(teste$saga_wetness_index, range = c(5,14), add = T, col =grDevices::topo.colors((50) ))
 
-
+gower_list
